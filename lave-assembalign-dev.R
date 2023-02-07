993f1f279f59cc02d64d6f1972be6137c9d12ea6
JohnUrban <mr.john.urban@gmail.com>
2023-02-07 15:10:58 -0500

    Adding control over border width for gaps/bed aln. Take 2.

--- lave-assembalign-dev.R
+++ lave-assembalign-dev.R
@@ -411,11 +411,13 @@ assembalign.dotplot <- function(querylengths=NA, targetlengths=NA, paf=NA, targe
   if(sum(is.na(querygaps)) == 0){gapaln(tigs=querylengths, gaps=querygaps, top=xlim[1]*gappy.scale[1], 
                                         bottom=xlim[1]*gappy.scale[2], col=gapcol, border=gapbcol, 
                                         scale = querylengthnorm, yaxisAln = TRUE) #, polygon.border.width=gapborderwidth)}
+  }
   if(sum(is.na(targetgaps)) == 0){gapaln(tigs=targetlengths, gaps=targetgaps, top=xlim[1]*gappy.scale[1], 
                                          bottom=xlim[1]*gappy.scale[2], col=gapcol, border=gapbcol, 
                                          scale=targetlengthnorm) #, polygon.border.width==gapborderwidth)}
+  }
 }
-
+  
 
 
 
